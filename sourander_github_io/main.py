d66d39004340c15460477804c6f38b9c4fcd00e7
Jani Sourander <jani.sourander@kamk.fi>
2024-02-12 15:25:47 +0200

    Modulerized code and added script write-index

--- sourander_github_io/main.py
+++ sourander_github_io/main.py
@@ -1,27 +1,35 @@
 import requests
 
+from sourander_github_io.models.github import Site
 from pathlib import Path
 from jinja2 import Environment, FileSystemLoader
 
-from sourander_github_io.models.github import Site
+def fetch_sites(namespace="sourander") -> list[Site]:
+    # Fetch the repositories from the API
+    response = requests.get(f"https://api.github.com/users/{namespace}/repos")
+    repositories = response.json()
 
-# Fetch the repositories from the API
-response = requests.get("https://api.github.com/users/sourander/repos")
-repositories = response.json()
+    # Filter repositories with non-null homepage
+    filtered_repositories = [r for r in repositories if r["homepage"] is not None]
+    filtered_repositories = [x for x in filtered_repositories if "github.io" in x.get("homepage", "")]
 
-# Filter repositories with non-null homepage
-filtered_repositories = [r for r in repositories if r["homepage"] is not None]
-filtered_repositories = [x for x in filtered_repositories if "github.io" in x.get("homepage", "")]
+    assert len(filtered_repositories), "No GitHub Pages repositories found!"
 
-# Create the sites list
-sites = [Site(name=repo["name"], description=repo["description"]) for repo in filtered_repositories]
+    # Create the sites list
+    return [Site(name=repo["name"], description=repo["description"]) for repo in filtered_repositories]
 
-# Load the Jinja2 template
-env = Environment(loader=FileSystemLoader('templates/'))
-template = env.get_template('index.template.md')
 
-# Render the template with the provided data
-output = template.render(sites=sites)
+def write_index():
+    # Load the Jinja2 template
+    env = Environment(loader=FileSystemLoader('templates/'))
+    template = env.get_template('index.template.md')
 
-output_file = Path("docs/index.md")
-output_file.write_text(output)
+    # Render the template with the provided data
+    sites = fetch_sites()
+    output = template.render(sites=sites)
+
+    output_file = Path("docs/index.md")
+    output_file.write_text(output)
+
+if __name__ == "__main__":
+    write_index()
