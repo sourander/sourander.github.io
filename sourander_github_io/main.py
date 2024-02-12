b56b216fb7b5c77191c2a8cde7275fd8cb7d3265
Jani Sourander <jani.sourander@kamk.fi>
2024-02-12 15:12:06 +0200

    Keep only github.io sites

--- sourander_github_io/main.py
+++ sourander_github_io/main.py
@@ -10,7 +10,8 @@ response = requests.get("https://api.github.com/users/sourander/repos")
 repositories = response.json()
 
 # Filter repositories with non-null homepage
-filtered_repositories = [repo for repo in repositories if repo["homepage"] is not None]
+filtered_repositories = [r for r in repositories if r["homepage"] is not None]
+filtered_repositories = [x for x in filtered_repositories if "github.io" in x.get("homepage", "")]
 
 # Create the sites list
 sites = [Site(name=repo["name"], description=repo["description"]) for repo in filtered_repositories]
@@ -21,5 +22,6 @@ template = env.get_template('index.template.md')
 
 # Render the template with the provided data
 output = template.render(sites=sites)
+
 output_file = Path("docs/index.md")
 output_file.write_text(output)
